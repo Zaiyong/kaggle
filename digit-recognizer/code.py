8cfaadc74d2ae2f55b4bff1edb9f35993675fcf0
Zaiyong <zhangqibiao177@hotmail.com>
2017-09-26 10:28:32 +0200

    Fix some bugs

--- digit-recognizer/code.py
+++ digit-recognizer/code.py
@@ -7,7 +7,7 @@ from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 def loadTrain(train_file,test_file):
     sub_df=pd.read_csv(train_file)
-    train_X_df, test_X_df, train_Y_df, test_Y_df=train_test_split(sub_df.iloc[:,1:],sub_df['label'],test_size=0.3,stratify=sub_df['label'])
+    train_X_df, test_X_df, train_Y_df, test_Y_df=train_test_split(sub_df.iloc[:,1:],sub_df['label'],test_size=0.2,stratify=sub_df['label'])
     print(train_X_df.shape)
     print(train_Y_df.shape)
     print(test_X_df.shape)
@@ -51,7 +51,7 @@ def trainGradientBoostingModel(X,Y,test_X,test_Y):
     evaluateModel(clf,test_X,test_Y)
 
 if __name__=='__main__':
-    train_X,train_Y,test_X,test_Y=loadTrain('./train.csv')
+    train_X,train_Y,test_X,test_Y=loadTrain('./train.csv','./test.csv')
     # print('train svm')
     # trainSVMModel(train_X,train_Y,test_X,test_Y)
     # print('train rf')
